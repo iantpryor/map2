1eafc4f5406253e4f95b0c678c57290671c7d191
iantpryor <iantpryor@gmail.com>
2015-04-06 12:17:54 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,34 +15,34 @@
                 case up:
                     switch(leftright){
                         case left:
-                            //10, 11, 0, 1
+                            //18, 19, 0, 1
                             break;
                         case right:
-                            //1, 2, 3, 4
+                            //3, 4, 5, 6
                             break;
                         default:
-                        //0, 1, 2
+                        //1, 2 ,3
                     }
                     break;
                 case down:
                     switch(leftright){
                         case left:
-                            //7, 8, 9, 10
+                            //13, 14, 15, 16
                             break;
                         case right:
-                            //4, 5, 6, 7
+                            //8, 9, 10, 11
                             break;
                         default:
-                        //6, 7, 8
+                        //11, 12, 13
                     }
                     break;
                 default:
                     switch(leftright){
                         case left:
-                            //9, 10, 11
+                            //16, 17, 18
                             break;
                         case right:
-                            //3, 4, 5
+                            //6, 7, 8
                             break;
                     }
             }
